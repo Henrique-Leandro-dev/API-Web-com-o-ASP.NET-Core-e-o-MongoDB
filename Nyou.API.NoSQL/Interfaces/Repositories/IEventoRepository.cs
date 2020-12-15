1a1344c1d5e60ce3c6f11992fde0307438ee6625
Henrique Leandro <miniqui10@gmail.com>
2020-12-15 08:07:11 -0200

    Testantando AspNetCore com Mongo

---
+++ Nyou.API.NoSQL/Interfaces/Repositories/IEventoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Nyou.API.NoSQL.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nyou.API.NoSQL.Interfaces.Repositories
+{
+   public interface IEventoRepository
+    {
+        List<EventoDomain> Listar();
+
+        EventoDomain BuscarPorId(string Id);
+
+        void Adicionar(EventoDomain evento);
+
+        void Atualizar(string id, EventoDomain evento);
+
+        void Remover(string id);
+    }
+}
